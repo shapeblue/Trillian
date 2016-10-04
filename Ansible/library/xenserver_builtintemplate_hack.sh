dc9a9ca9082fe11145fa79447055c1cdc1b30f31
Paul Angus <paul.angus@shapeblue.com>
2016-10-04 11:01:24 +0100

    fixup for xenserver satus hack

--- Ansible/library/xenserver_builtintemplate_hack.sh
+++ Ansible/library/xenserver_builtintemplate_hack.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+timeout=20
+active="true"
+while [[ "$active" == "true" ]]; do
+active="false"
+echo "Still seeing writes"
+if inotifywait -e modify $1/template/tmpl/1/5/ -t $timeout; then active="true" ; fi
+done
+echo "nothing was written in the directory for $timeout seconds.
+removing any extra VHDs"
 counter=1
 for file in $1/template/tmpl/1/5/*.vhd; do
- if [[ $((counter)) -gt 1 ]; then
+ if [[ $((counter)) > 1 ]]; then
     rm -f $file
+    echo "$file removed"
  fi
  counter=$((counter+1))
 done
