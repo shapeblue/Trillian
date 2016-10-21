2fba7d382e4b4a0b5e7678a0b1dd591a7e894777
Dag Sonstebo <dag.sonstebo@shapeblue.com>
2016-10-21 16:42:11 +0100

    Var troubleshooting.

---
+++ utils/evalansiblevars.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Evaluates Ansible template files againsts variable files
+# First arg is the template file
+
+IFS=$'\n';
+for i in `grep -ho '{{[a-zA-Z \-\_]*}}' $1 | sort | uniq`;
+do
+  tmplvarname=`echo ${i} | sed 's/{{//' | sed 's/}}//' | sed 's/^\ *//' | sed 's/^\ *$//'`;
+  foundvar=`grep -hi ${tmplvarname} $2`;
+  echo "${tmplvarname} matches: ${foundvar};
+done
