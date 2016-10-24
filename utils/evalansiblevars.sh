57a7f37cb35cf9a835b79f0c267e7d349b23e0e6
Dag Sonstebo <dag.sonstebo@shapeblue.com>
2016-10-24 11:55:28 +0100

    Complete.

--- utils/evalansiblevars.sh
+++ utils/evalansiblevars.sh
@@ -4,9 +4,9 @@
 # $2 is the ansible hostfile
 # $3 is the ansible hostname
 IFS=$'\n';
-echo -e "\nVariable file: $1";
+echo -e "\nTemplate file: $1";
 echo -e "Host file:     $2";
-echo -e "Host:          $3\n\n";
+echo -e "Host:          $3\n";
 
 for i in `grep -ho '{{[^}]*}}' $1 | sort | uniq`;
 do
