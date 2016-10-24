d63e4f8bfcd8f7d0db893d10a34b5fc82a1c7df0
Dag Sonstebo <dag.sonstebo@shapeblue.com>
2016-10-24 11:34:13 +0100

    sadf

--- utils/evalansiblevars.sh
+++ utils/evalansiblevars.sh
@@ -5,12 +5,14 @@
 IFS=$'\n';
 for i in `grep -ho '{{[^}]*}}' $1 | sort | uniq`;
 do
-  foundvar="";
-  tmplvarname=`echo ${i} | sed 's/{*//' | sed 's/}*//' | sed 's/^\ *//' | sed 's/\ *$//'`;
-  foundvar=`grep -hi ^${tmplvarname} $2`;
-  if [ -z ${foundvar} ];
-  then
-    foundvar="NOTFOUND";
-  fi
-  echo "${tmplvarname} matches: ${foundvar}";
+  #foundvar="";
+  #tmplvarname=`echo ${i} | sed 's/{*//' | sed 's/}*//' | sed 's/^\ *//' | sed 's/\ *$//'`;
+  #foundvar=`grep -hi ^${tmplvarname} $2`;
+  #if [ -z ${foundvar} ];
+  #then
+  #    foundvar="NOTFOUND";
+  #fi
+  #echo "${tmplvarname} matches: ${foundvar}";
+  cleanvarname=`echo ${i} | sed 's/{*//' | sed 's/}*//' | sed 's/^\ *//' | sed 's/\ *$//'`;
+  ansible -m debug -a "var=${cleanvarname}" -i $2 $3;
 done
