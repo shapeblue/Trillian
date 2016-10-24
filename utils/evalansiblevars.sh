265ab62782aab9d29cc67123befcc60e761ca621
Dag Sonstebo <dag.sonstebo@shapeblue.com>
2016-10-24 11:45:10 +0100

    Works!

--- utils/evalansiblevars.sh
+++ utils/evalansiblevars.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 # Evaluates Ansible template files againsts variable files
-# First arg is the template file
-
+# $1 is the template file
+# $2 is the ansible hostfile
+# $3 is the ansible hostname
 IFS=$'\n';
 for i in `grep -ho '{{[^}]*}}' $1 | sort | uniq`;
 do
-  #foundvar="";
-  #tmplvarname=`echo ${i} | sed 's/{*//' | sed 's/}*//' | sed 's/^\ *//' | sed 's/\ *$//'`;
-  #foundvar=`grep -hi ^${tmplvarname} $2`;
-  #if [ -z ${foundvar} ];
-  #then
-  #    foundvar="NOTFOUND";
-  #fi
-  #echo "${tmplvarname} matches: ${foundvar}";
   cleanvarname=`echo ${i} | sed 's/{*//' | sed 's/}}//' | sed 's/^\ *//' | sed 's/\ *$//' | sed 's/|\ lower//'`;
   ansible -m debug -a "var=${cleanvarname}" -i $2 $3
 done
