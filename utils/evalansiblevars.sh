42174d310158d7475dc6b82f11da699184b1bd95
Dag Sonstebo <dag.sonstebo@shapeblue.com>
2016-10-24 09:09:04 +0100

    Additional regexp options.

--- utils/evalansiblevars.sh
+++ utils/evalansiblevars.sh
@@ -3,14 +3,14 @@
 # First arg is the template file
 
 IFS=$'\n';
-for i in `grep -ho '{{[a-zA-Z \-\_]*}}' $1 | sort | uniq`;
+for i in `grep -ho '{{[a-zA-Z \-\_\[\]\.\']*}}' $1 | sort | uniq`;
 do
   foundvar="";
-  tmplvarname=`echo ${i} | sed 's/{{//' | sed 's/}}//' | sed 's/^\ *//' | sed 's/\ *$//'`;
+  tmplvarname=`echo ${i} | sed 's/{*//' | sed 's/}*//' | sed 's/^\ *//' | sed 's/\ *$//'`;
   foundvar=`grep -hi ^${tmplvarname} $2`;
-  #if [ -z ${foundvar} ];
-  #then
-  #  foundvar="NOTFOUND";
-  #fi
+  if [ -z ${foundvar} ];
+  then
+    foundvar="NOTFOUND";
+  fi
   echo "${tmplvarname} matches: ${foundvar}";
 done
