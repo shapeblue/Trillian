26de919b5419777690fa89827481965042394f72
Paul Angus <paul.angus@shapeblue.com>
2017-08-22 21:05:10 +0100

    improve jenkins upload

--- Ansible/roles/marvin/files/upload_to_es.sh
+++ Ansible/roles/marvin/files/upload_to_es.sh
@@ -3,9 +3,11 @@
 
 ES_IP="10.4.0.15"
 RESULTS_INDEX="test_results"
-ENVDATA_INDEX="env_cfg_file.json"
+ENV_CFG_DATA="env_cfg_file.json"
 ENV_TEST_DATA="env_test_data.json"
 ENV_EXTRA_DATA="env_extra_data.json"
+
+cd /marvin/json_results
 
 echo " -- Upload test results"
 for file in ./test_*.json; do
@@ -13,7 +15,7 @@ for file in ./test_*.json; do
 done
 
 echo " -- Upload test env cfg"
-curl -XPOST "$ES_IP:9200/$RESULTS_INDEX/external?pretty" -d @$ENVDATA_INDEX
+curl -XPOST "$ES_IP:9200/$RESULTS_INDEX/external?pretty" -d @$ENV_CFG_DATA
 
 echo " -- Upload test data used"
 curl -XPOST "$ES_IP:9200/$RESULTS_INDEX/external?pretty" -d @$ENV_TEST_DATA
