1b58a428bf6a6b930767125e7d180a668852c349
Paul Angus <paul.angus@shapeblue.com>
2017-12-05 09:08:54 +0000

    major refactor to produce multiple files

--- Ansible/roles/marvin/files/upload_to_es.sh
+++ Ansible/roles/marvin/files/upload_to_es.sh
@@ -4,9 +4,11 @@
 ES_IP="10.4.0.15"
 RESULTS_INDEX="test_results"
 ENV_TEST_DATA="./test_run.json"
-
+JSON_OUTPUT_DIR="/marvin/json_results"
 
 cd /marvin
 
 echo " -- Upload test results"
-curl -XPOST "$ES_IP:9200/$RESULTS_INDEX/external?pretty" -d @$ENV_TEST_DATA
+for file in $JSON_OUTPUT_DIR/test_*.json; do
+curl -XPOST "$ES_IP:9200/$RESULTS_INDEX/external?pretty" -d @$file
+done
