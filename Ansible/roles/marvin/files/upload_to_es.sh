d105bcde3ac11920521424b8b57b7754adb03339
Paul Angus <paul.angus@shapeblue.com>
2018-01-23 16:14:18 +0000

    upload metadata to separate index

--- Ansible/roles/marvin/files/upload_to_es.sh
+++ Ansible/roles/marvin/files/upload_to_es.sh
@@ -5,6 +5,8 @@ ES_IP="10.4.0.15"
 RESULTS_INDEX="test_results"
 ENV_TEST_DATA="./test_run.json"
 JSON_OUTPUT_DIR="/marvin/json_results"
+TESTRUN_INDEX="test_runs"
+metafile="$JSON_OUTPUT_DIR/additional_test_data.json"
 
 cd /marvin
 
@@ -12,3 +14,8 @@ echo " -- Upload test results"
 for file in $JSON_OUTPUT_DIR/test_*.json; do
 curl -XPOST "$ES_IP:9200/$RESULTS_INDEX/external?pretty" -d @$file
 done
+
+
+echo " -- Upload test metadata"
+curl -XPOST "$ES_IP:9200/$TESTRUN_INDEX/external?pretty" -d @$metafile
+
