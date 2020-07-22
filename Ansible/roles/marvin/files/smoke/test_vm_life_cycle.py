f1b15e13b52963493488c129be67ff8f3153784d
Abhishek Kumar <abhishek.mrt22@gmail.com>
2020-07-22 05:04:39 +0000

    use test template for XCP-ng/Xenserver (#95)
    * use test template for XCP-ng/Xenserver
    
    Signed-off-by: Abhishek Kumar <abhishek.mrt22@gmail.com>
    
    * ptiny skip mounting message
    
    Signed-off-by: Abhishek Kumar <abhishek.mrt22@gmail.com>
    
    * review changes
    
    Signed-off-by: Abhishek Kumar <abhishek.mrt22@gmail.com>
    
    * indentation
    
    Signed-off-by: Abhishek Kumar <abhishek.mrt22@gmail.com>
    
    * indentation
    
    Signed-off-by: Abhishek Kumar <abhishek.mrt22@gmail.com>

--- Ansible/roles/marvin/files/smoke/test_vm_life_cycle.py
+++ Ansible/roles/marvin/files/smoke/test_vm_life_cycle.py
@@ -34,8 +34,9 @@ from marvin.lib.base import (Account,
                              Router,
                              Configurations)
 from marvin.lib.common import (get_domain,
-                                get_zone,
-                                get_template,
+                               get_zone,
+                               get_template,
+                               get_test_template,
                                list_hosts)
 from marvin.codes import FAILED, PASS
 from nose.plugins.attrib import attr
@@ -44,6 +45,21 @@ import time
 import re
 
 _multiprocess_shared_ = True
+
+def getTemplateForTest(apiclient, zoneid, ostypeid, hypervisor):
+    template = FAILED
+    if hypervisor.lower() in ["xenserver"]:
+        template = get_test_template(
+            apiclient,
+            zoneid,
+            hypervisor)
+    if template == FAILED:
+        template = get_template(
+            apiclient,
+            zone.id,
+            ostypeid)
+    return template
+
 class Test01DeployVM(cloudstackTestCase):
 
     @classmethod
@@ -56,6 +72,7 @@ class Test01DeployVM(cloudstackTestCase):
         cls.domain = get_domain(cls.apiclient)
         cls.zone = get_zone(cls.apiclient, testClient.getZoneForTests())
         cls.services['mode'] = cls.zone.networktype
+        cls.hypervisor = testClient.getHypervisorInfo()
 
         #If local storage is enabled, alter the offerings to use localstorage
         #this step is needed for devcloud
@@ -64,11 +81,7 @@ class Test01DeployVM(cloudstackTestCase):
             cls.services["service_offerings"]["small"]["storagetype"] = 'local'
             cls.services["service_offerings"]["medium"]["storagetype"] = 'local'
 
-        template = get_template(
-            cls.apiclient,
-            cls.zone.id,
-            cls.services["ostype"]
-        )
+        template = getTemplateForTest(cls.apiclient, cls.zone.id, cls.services["ostype"], cls.hypervisor)
         if template == FAILED:
             assert False, "get_template() failed to return template with description %s" % cls.services["ostype"]
 
@@ -275,11 +288,7 @@ class Test02VMLifeCycle(cloudstackTestCase):
             cls.services["service_offerings"]["small"]["storagetype"] = 'local'
             cls.services["service_offerings"]["medium"]["storagetype"] = 'local'
 
-        template = get_template(
-                            cls.apiclient,
-                            cls.zone.id,
-                            cls.services["ostype"]
-                            )
+        template = getTemplateForTest(cls.apiclient, cls.zone.id, cls.services["ostype"], cls.hypervisor)
         if template == FAILED:
             assert False, "get_template() failed to return template with description %s" % cls.services["ostype"]
 
@@ -732,57 +741,63 @@ class Test02VMLifeCycle(cloudstackTestCase):
         cmds = "mkdir -p %s" % mount_dir
         self.assert_(ssh_client.execute(cmds) == [], "mkdir failed within guest")
 
+        iso_unsupported = False
         for diskdevice in self.services["diskdevice"]:
             res = ssh_client.execute("mount -rt iso9660 {} {}".format(diskdevice, mount_dir))
             if res == []:
                 self.services["mount"] = diskdevice
                 break
+            if str(res).find("mount: unknown filesystem type 'iso9660'") != -1:
+                iso_unsupported = True
+                log_msg = "Test template does not supports iso9660 filesystem. Proceeding with test without mounting."
+                self.debug(log_msg)
+                print log_msg
+                break
         else:
             self.fail("No mount points matched. Mount was unsuccessful")
 
-        c = "mount |grep %s|head -1" % self.services["mount"]
-        res = ssh_client.execute(c)
-        size = ssh_client.execute("du %s | tail -1" % self.services["mount"])
-        self.debug("Found a mount point at %s with size %s" % (res, size))
+        if iso_unsupported == False:
+            c = "mount |grep %s|head -1" % self.services["mount"]
+            res = ssh_client.execute(c)
+            size = ssh_client.execute("du %s | tail -1" % self.services["mount"])
+            self.debug("Found a mount point at %s with size %s" % (res, size))
 
-        # Get ISO size
-        iso_response = Iso.list(
-                                 self.apiclient,
-                                 id=iso.id
-                                 )
-        self.assertEqual(
-                            isinstance(iso_response, list),
-                            True,
-                            "Check list response returns a valid list"
-                        )
+            # Get ISO size
+            iso_response = Iso.list(
+                self.apiclient,
+                id=iso.id)
+            self.assertEqual(
+                isinstance(iso_response, list),
+                True,
+                "Check list response returns a valid list")
 
-        try:
-            #Unmount ISO
-            command = "umount %s" % mount_dir
-            ssh_client.execute(command)
-        except Exception as e:
-            self.fail("SSH failed for virtual machine: %s - %s" %
-                                (self.virtual_machine.ipaddress, e))
+            try:
+                # Unmount ISO
+                command = "umount %s" % mount_dir
+                ssh_client.execute(command)
+            except Exception as e:
+                self.fail("SSH failed for virtual machine: %s - %s" %
+                    (self.virtual_machine.ipaddress, e))
 
-        #Detach from VM
+        # Detach from VM
         cmd = detachIso.detachIsoCmd()
         cmd.virtualmachineid = self.virtual_machine.id
         self.apiclient.detachIso(cmd)
 
-        try:
-            res = ssh_client.execute(c)
-        except Exception as e:
-            self.fail("SSH failed for virtual machine: %s - %s" %
-                                (self.virtual_machine.ipaddress, e))
+        if iso_unsupported == False:
+            try:
+                res = ssh_client.execute(c)
+            except Exception as e:
+                self.fail("SSH failed for virtual machine: %s - %s" %
+                    (self.virtual_machine.ipaddress, e))
 
-        # Check if ISO is properly detached from VM (using fdisk)
-        result = self.services["mount"] in str(res)
+            # Check if ISO is properly detached from VM (using fdisk)
+            result = self.services["mount"] in str(res)
 
-        self.assertEqual(
-                         result,
-                         False,
-                         "Check if ISO is detached from virtual machine"
-                         )
+            self.assertEqual(
+                result,
+                False,
+                "Check if ISO is detached from virtual machine")
         return
 
 class Test03SecuredVmMigration(cloudstackTestCase):
@@ -805,11 +820,7 @@ class Test03SecuredVmMigration(cloudstackTestCase):
         cls.hostConfig = cls.config.__dict__["zones"][0].__dict__["pods"][0].__dict__["clusters"][0].__dict__["hosts"][0].__dict__
         cls.management_ip = cls.config.__dict__["mgtSvr"][0].__dict__["mgtSvrIp"]
 
-        template = get_template(
-                            cls.apiclient,
-                            cls.zone.id,
-                            cls.services["ostype"]
-                            )
+        template = getTemplateForTest(cls.apiclient, cls.zone.id, cls.services["ostype"], cls.hypervisor)
         if template == FAILED:
             assert False, "get_template() failed to return template with description %s" % cls.services["ostype"]
 
