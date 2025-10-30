380d6f3db9821478b1160e4872bbdf6959f9b987
Rositsa Kyuchukova <rositsa.kyuchukova@shapeblue.com>
2025-10-30 21:59:03 +0200

    Fix: Pre-populate missing password variables from def_ versions
    The template expects password variables like mgmtsrv_password but these
    are not set in the vars. Instead, only def_mgmtsrv_password exists.
    Pre-populate all password variables from their def_ versions to ensure
    passwords flow through to the inventory file.
    
    Fixes: Empty ansible_ssh_pass in generated inventory

--- Ansible/files/process_inventory_template.py
+++ Ansible/files/process_inventory_template.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 import json
 import sys
-from jinja2 import Environment, FileSystemLoader
+from jinja2 import Environment, FileSystemLoader, Undefined
 
 # Get command line arguments
 vars_file = sys.argv[1]      # JSON file with variables
@@ -10,7 +10,43 @@ output_file = sys.argv[3]    # Output file path
 
 # Load variables from JSON
 with open(vars_file, 'r') as f:
-    vars_dict = json.load(f)
+    vars_dict = json.load(f)\
+    
+# Pre-populate missing password variables with their def_ versions
+password_vars = [
+    'mgmtsrv_password',
+    'kvm_password', 
+    'xs_password',
+    'vmware_esxi_password',
+    'dbsrv_password',
+    'marvin_password',
+    'playwright_password',
+    'pri_password',
+    'pri_password_iscsi',
+    'sec_password'
+]
+
+print("=== Password Variable Population Debug ===")
+for var in password_vars:
+    if var not in vars_dict or not vars_dict[var]:
+        def_var = 'def_' + var
+        if def_var in vars_dict:
+            print("Populating {} from {} = {}".format(var, def_var, vars_dict[def_var]))
+            vars_dict[var] = vars_dict[def_var]
+        else:
+            print("WARNING: {} not in vars_dict and {} not found".format(var, def_var))
+    else:
+        print("OK: {} already set to {}".format(var, vars_dict[var]))
+print("=== End Password Debug ===")
+
+class SilentUndefined(Undefined):
+    """Return None for undefined variables instead of raising errors"""
+    def _fail_with_undefined_error(self, *args, **kwargs):
+        return None
+    def __str__(self):
+        return ''
+    def __bool__(self):
+        return False
 
 # Define custom filters to match Ansible's behavior
 def bool_filter(value):
@@ -32,7 +68,7 @@ def default_filter(value, default='', boolean=False):
     return value if value is not None else default
 
 # Setup Jinja2 with custom filters
-env = Environment(loader=FileSystemLoader('.'))
+env = Environment(loader=FileSystemLoader('.'), undefined=SilentUndefined)
 env.filters['bool'] = bool_filter
 env.filters['to_json'] = to_json_filter
 env.filters['default'] = default_filter
